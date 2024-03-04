85aef5c48adad2a0aff2507ed7c7f5e453573b22
alcino <alcino.jssousa@gmail.com>
2024-03-04 18:58:14 +0000

    chore! : added primeng;

--- comprarempresa/src/app/app.component.ts
+++ comprarempresa/src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Calendar } from 'primeng/calendar';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'comprarempresa';
+  date: Date = new Date();
+  
+  ngOnInit(): void {
+    
+  }
 }
