82df4eaeb350c9e7b6eab7f97665fed684d23199
alcino <alcino.jssousa@gmail.com>
2024-03-08 20:14:55 +0000

    chore: ! added basic structure listings

--- comprarempresa/src/app/app.component.ts
+++ comprarempresa/src/app/app.component.ts
@@ -1,17 +1,113 @@
 import { Component, OnInit } from '@angular/core';
-import { Calendar } from 'primeng/calendar';
+
+interface BusinessAd {
+    name: string,
+    address: string,
+    type: string,
+    price: string,
+    picture: string
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  
-  title = 'comprarempresa';
-  date: Date = new Date();
-  
-  ngOnInit(): void {
-    
-  }
+    rangeValues: number[] = [20, 80];
+
+    businessAds!: BusinessAd[];
+
+    constructor() { }
+
+    ngOnInit() {
+        this.businessAds = [
+            {
+                name: 'Primavera em Flor',
+                address: 'Coimbra',
+                type: 'Floricultura',
+                price: '6 000€',
+                picture: '/assets/ads-pictures/flower-shop.jpg'
+            },
+            {
+                name: 'Cortes & Estilos',
+                address: 'Porto',
+                type: 'Cabeleireiro',
+                price: '10 000€',
+                picture: '/assets/ads-pictures/flower-shop.jpg'
+
+            },
+            {
+                name: 'Páginas Encantadas',
+                address: 'Lisboa',
+                type: 'Livraria',
+                price: '10 000€',
+                picture: '/assets/ads-pictures/flower-shop.jpg'
+            },
+            {
+                name: 'Cantinho dos Sabores',
+                address: 'Aveiro',
+                type: 'Mercearia',
+                price: '13 000€',
+                picture: '/assets/ads-pictures/flower-shop.jpg'
+            },
+            {
+                name: 'TransGlobal',
+                address: 'Vila Real',
+                type: 'Transportes',
+                price: '16 000€',
+                picture: '/assets/ads-pictures/flower-shop.jpg'
+            },
+            {
+                name: 'Sabores de Portugal',
+                address: 'Lisboa',
+                type: 'Restaurante',
+                price: '20 000€',
+                picture: '/assets/ads-pictures/flower-shop.jpg'
+            },
+            {
+                name: 'AtivaFit',
+                address: 'Porto',
+                type: 'Ginásio',
+                price: '25 000€',
+                picture: '/assets/ads-pictures/flower-shop.jpg'
+            },
+            {
+                name: 'Fundação Forte',
+                address: 'Lisboa',
+                type: 'Construção',
+                price: '40 000€',
+                picture: '/assets/ads-pictures/flower-shop.jpg'
+            },
+            {
+                name: 'Primavera em Flor',
+                address: 'Coimbra',
+                type: 'Floricultura',
+                price: '6 000€',
+                picture: '/assets/ads-pictures/flower-shop.jpg'
+            },
+            {
+                name: 'Primavera em Flor',
+                address: 'Coimbra',
+                type: 'Floricultura',
+                price: '6 000€',
+                picture: '/assets/ads-pictures/flower-shop.jpg'
+            },
+            {
+                name: 'Primavera em Flor',
+                address: 'Coimbra',
+                type: 'Floricultura',
+                price: '6 000€',
+                picture: '/assets/ads-pictures/flower-shop.jpg'
+            },
+            {
+                name: 'Primavera em Flor',
+                address: 'Coimbra',
+                type: 'Floricultura',
+                price: '6 000€',
+                picture: '/assets/ads-pictures/flower-shop.jpg'
+            }
+        ];
+    }
 }
+
--- comprarempresa/src/app/app.module.ts
+++ comprarempresa/src/app/app.module.ts
@@ -3,12 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CalendarModule } from 'primeng/calendar';
 import { AppComponent } from './app.component';
+import { MenubarModule } from 'primeng/menubar';
+import { MegaMenuModule } from 'primeng/megamenu';
+import { DataViewModule } from 'primeng/dataview';
+import { RatingModule } from 'primeng/rating';
+import { TagModule } from 'primeng/tag';
+import { FormsModule } from '@angular/forms';
+import { AccordionModule } from 'primeng/accordion';
+import { SliderModule } from 'primeng/slider';
+import { InputNumberModule } from 'primeng/inputnumber';
 
 @NgModule({
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
-        CalendarModule
+        CalendarModule,
+        MenubarModule,
+        MegaMenuModule,
+        DataViewModule,
+        RatingModule,
+        TagModule,
+        FormsModule,
+        AccordionModule,
+        SliderModule,
+        InputNumberModule
     ],
     declarations: [
         AppComponent
