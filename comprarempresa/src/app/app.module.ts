77443e3b7c41842a19fdeb4270344d60baa9b59c
alcino <alcino.jssousa@gmail.com>
2024-03-09 15:06:50 +0000

    chore! : init github pages

--- comprarempresa/src/app/app.module.ts
+++ comprarempresa/src/app/app.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CalendarModule } from 'primeng/calendar';
 import { AppComponent } from './app.component';
-import { MenubarModule } from 'primeng/menubar';
-import { MegaMenuModule } from 'primeng/megamenu';
 import { DataViewModule } from 'primeng/dataview';
-import { RatingModule } from 'primeng/rating';
-import { TagModule } from 'primeng/tag';
 import { FormsModule } from '@angular/forms';
 import { AccordionModule } from 'primeng/accordion';
 import { SliderModule } from 'primeng/slider';
@@ -17,12 +12,7 @@ import { InputNumberModule } from 'primeng/inputnumber';
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
-        CalendarModule,
-        MenubarModule,
-        MegaMenuModule,
         DataViewModule,
-        RatingModule,
-        TagModule,
         FormsModule,
         AccordionModule,
         SliderModule,
